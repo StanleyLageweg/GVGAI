160a58d28783b378d9951ac8130f20c0f4c69efd
Stanley Lageweg <s.lageweg-1@student.tudelft.nl>
2019-07-18 16:56:49 +0200

    SpriteCounter

---
+++ test/username/SpriteCounterTest.java
@@ -0,0 +1,87 @@
+package username;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SpriteCounterTest {
+
+	private static final String SPRITE = "sprite";
+
+	private static final List<LogRecord> LOG_RECORDS = new TestLogHandler(Constants.LOGGER).getRecords();
+
+	private SpriteCounter spriteCounter;
+
+	@BeforeEach
+	void beforeEach() {
+		spriteCounter = new SpriteCounter();
+		LOG_RECORDS.clear();
+	}
+
+	@Test
+	void zero() {
+		assertThat(spriteCounter.get(SPRITE)).isZero();
+	}
+
+	@Test
+	void increment() {
+		// Increment
+		spriteCounter.increment(SPRITE);
+
+		// Assert count changed
+		assertThat(spriteCounter.get(SPRITE)).isOne();
+	}
+
+	@Test
+	void increment3Times() {
+		// Increment 3 times
+		spriteCounter.increment(SPRITE);
+		spriteCounter.increment(SPRITE);
+		spriteCounter.increment(SPRITE);
+
+		// Assert count changed
+		assertThat(spriteCounter.get(SPRITE)).isEqualTo(3);
+	}
+
+	@Test
+	void decrementZero() {
+		// Decrement
+		spriteCounter.decrement(SPRITE);
+
+		// Assert count did not change
+		assertThat(spriteCounter.get(SPRITE)).isZero();
+
+		// Assert one warning was logged
+		assertThat(LOG_RECORDS).extracting(LogRecord::getLevel).containsExactly(Level.WARNING);
+	}
+
+	@Test
+	void decrementOne() {
+		// Increment
+		spriteCounter.increment(SPRITE);
+
+		// Decrement
+		spriteCounter.decrement(SPRITE);
+
+		// Assert count changed
+		assertThat(spriteCounter.get(SPRITE)).isZero();
+	}
+
+	@Test
+	void decrementTwo() {
+		// Increment 2 times
+		spriteCounter.increment(SPRITE);
+		spriteCounter.increment(SPRITE);
+
+		// Decrement
+		spriteCounter.decrement(SPRITE);
+
+		// Assert count changed
+		assertThat(spriteCounter.get(SPRITE)).isOne();
+	}
+}
